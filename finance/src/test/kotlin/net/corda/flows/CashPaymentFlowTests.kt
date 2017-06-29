5422598a47432be691e03e69b590a47c8ae05a08
Ross Nicoll <ross.nicoll@r3.com>
2017-06-29 14:01:42 +0100

    Rename TxKeyFlow to TransactionKeyFlow

--- finance/src/test/kotlin/net/corda/flows/CashPaymentFlowTests.kt
+++ finance/src/test/kotlin/net/corda/flows/CashPaymentFlowTests.kt
@@ -32,7 +32,7 @@ class CashPaymentFlowTests {
         notary = notaryNode.info.notaryIdentity
         bankOfCorda = bankOfCordaNode.info.legalIdentity
 
-        notaryNode.registerInitiatedFlow(TxKeyFlow.Provider::class.java)
+        notaryNode.registerInitiatedFlow(TransactionKeyFlow.Provider::class.java)
         notaryNode.identity.registerIdentity(bankOfCordaNode.info.legalIdentityAndCert)
         bankOfCordaNode.identity.registerIdentity(notaryNode.info.legalIdentityAndCert)
         val future = bankOfCordaNode.services.startFlow(CashIssueFlow(initialBalance, ref,
